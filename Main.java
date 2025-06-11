891ceca65e62e09eca17c14ac81df1f9c8998025
Matheus Santana <matheussant1805@gmail.com>
2025-06-11 18:37:16 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
-import dao.CavaleiroDAO;
-import model.Cavaleiro;
+package dao;
 
+import model.Cavaleiro;
 import java.util.Scanner;
 
 public class Main {
@@ -8,26 +8,65 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         CavaleiroDAO dao = new CavaleiroDAO();
 
-        System.out.println("=== Menu CDZ - Saga Eliseos ===");
-        System.out.println("1. Cadastrar Cavaleiro");
+        while (true) {
+            System.out.println("\n=== Menu CDZ - Saga Eliseos ===");
+            System.out.println("1. Cadastrar Cavaleiro");
+            System.out.println("2. Listar Cavaleiros");
+            System.out.println("3. Editar Cavaleiro");
+            System.out.println("4. Excluir Cavaleiro");
+            System.out.println("0. Sair");
 
+            int opcao = sc.nextInt();
+            sc.nextLine();
 
-        int opcao = sc.nextInt();
-        sc.nextLine(); // limpar buffer
+            if (opcao == 1) {
+                Cavaleiro c = new Cavaleiro();
+                System.out.print("Nome: ");
+                c.setNome(sc.nextLine());
+                System.out.print("Constelação: ");
+                c.setConstelacao(sc.nextLine());
+                System.out.print("Nível: ");
+                c.setNivel(sc.nextLine());
+                System.out.print("Afiliacao: ");
+                c.setAfiliacao(sc.nextLine());
+                dao.criarCavaleiro(c);
+                System.out.println("Cavaleiro inserido!");
 
-        if (opcao == 1) {
-            Cavaleiro c = new Cavaleiro();
-            System.out.print("Nome: ");
-            c.setNome(sc.nextLine());
-            System.out.print("Constelação: ");
-            c.setConstelacao(sc.nextLine());
-            System.out.print("Nível: ");
-            c.setNivel(sc.nextLine());
-            System.out.print("Afiliacao: ");
-            c.setAfiliacao(sc.nextLine());
+            } else if (opcao == 2) {
+                for (Cavaleiro c : dao.listar()) {
+                    System.out.printf("[%d] %s - %s - %s - %s\n",
+                            c.getId(), c.getNome(), c.getConstelacao(),
+                            c.getNivel(), c.getAfiliacao());
+                }
 
-            dao.criarCavaleiro(c);
-            System.out.println("Cavaleiro inserido!");
+            } else if (opcao == 3) {
+                System.out.print("ID do cavaleiro a editar: ");
+                int id = sc.nextInt(); sc.nextLine();
+                Cavaleiro c = new Cavaleiro();
+                c.setId(id);
+                System.out.print("Novo Nome: ");
+                c.setNome(sc.nextLine());
+                System.out.print("Nova Constelação: ");
+                c.setConstelacao(sc.nextLine());
+                System.out.print("Novo Nível: ");
+                c.setNivel(sc.nextLine());
+                System.out.print("Nova Afiliacao: ");
+                c.setAfiliacao(sc.nextLine());
+                dao.atualizar(c);
+                System.out.println("Atualizado!");
+
+            } else if (opcao == 4) {
+                System.out.print("ID do cavaleiro a excluir: ");
+                int id = sc.nextInt();
+                dao.excluir(id);
+                System.out.println("Excluído!");
+
+            } else if (opcao == 0) {
+                System.out.println("Encerrando...");
+                break;
+            } else {
+                System.out.println("Opção inválida.");
+            }
         }
     }
 }

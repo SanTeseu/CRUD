bc5ccd7797c8d1504871758baa2da9a28fb59c4c
Matheus Santana <matheussant1805@gmail.com>
2025-06-11 18:36:38 -0300

    Update Cavaleiro.java

--- Cavaleiro.java
+++ Cavaleiro.java
@@ -7,4 +7,45 @@ public class Cavaleiro {
     private String nivel;
     private String afiliacao;
 
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getConstelacao() {
+        return constelacao;
+    }
+
+    public String getNivel() {
+        return nivel;
+    }
+
+    public String getAfiliacao() {
+        return afiliacao;
+    }
+
+    // Setters
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setConstelacao(String constelacao) {
+        this.constelacao = constelacao;
+    }
+
+    public void setNivel(String nivel) {
+        this.nivel = nivel;
+    }
+
+    public void setAfiliacao(String afiliacao) {
+        this.afiliacao = afiliacao;
+    }
 }
